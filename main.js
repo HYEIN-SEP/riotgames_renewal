46c98b3db6b92eae73fd059f016ea0fb72c8330a
HYEIN-SEP <hyeinpark22@gmail.com>
2023-06-28 07:36:14 +0900

    feat:자바스크립트 수정

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 let bottommenu = document.querySelectorAll(".bottom_menu li"),
-  nav = document.querySelector("nav");
+  nav = document.querySelector("nav"),
+  navList = document.querySelectorAll("nav ul");
 
 for (let i of bottommenu) {
   i.addEventListener("mouseover", () => {
     nav.classList.add("active");
   });
+}
+
+for (let i of navList) {
   i.addEventListener("mouseout", () => {
     nav.classList.remove("active");
   });
