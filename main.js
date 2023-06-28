c8a11efdb8b881086250de3784a3b00608f70b90
HYEIN-SEP <hyeinpark22@gmail.com>
2023-06-29 00:19:57 +0900

    feat:슬라이드 js추가, common.css수정

--- main.js
+++ main.js
@@ -13,6 +13,62 @@ nav.addEventListener("mouseout", () => {
   nav.classList.remove("active");
 });
 
+//슬라이드-혜인
+let slideWrapper = document.querySelector(".slide-wrapper"),
+  slideContainer = slideWrapper.querySelector(".slide-container"),
+  slides = slideContainer.querySelectorAll("li"),
+  slideCount = slides.length,
+  currentSlideIdx = 0,
+  prevBtn = slideWrapper.querySelector("#prev"),
+  nextBtn = slideWrapper.querySelector("#next");
+
+if (slideCount > 1) {
+  slides.forEach((item, idx) => {
+    item.style.left = `${idx * 100}%`;
+  });
+}
+
+function moveSlide(num) {
+  slideContainer.style.left = `${-num * 100}%`;
+  currentSlideIdx = num;
+
+  if (currentSlideIdx === slideCount - 1) {
+    nextBtn.classList.add("disabled");
+  } else {
+    nextBtn.classList.remove("disabled");
+  }
+  if (currentSlideIdx === 0) {
+    prevBtn.classList.add("disabled");
+  } else {
+    prevBtn.classList.remove("disabled");
+  }
+
+  // for (let item of slides) {
+  //   item.classList.remove("active");
+  // }
+  // slides[currentSlideIdx].classList.add("active");
+}
+moveSlide(0);
+
+nextBtn.addEventListener("click", () => {
+  if (currentSlideIdx < slideCount - 1) {
+    moveSlide(currentSlideIdx + 1);
+  }
+});
+prevBtn.addEventListener("click", () => {
+  if (currentSlideIdx > 0) {
+    moveSlide(currentSlideIdx - 1);
+  }
+});
+
+function autoslide() {
+  timer = setInterval(() => {
+    let nextIdx = (currentSlideIdx + 1) % slideCount;
+    moveSlide(nextIdx);
+  }, 3000);
+}
+autoslide();
+
 //탭-민용
 let tabMenu = document.querySelectorAll(".tab-menu li");
 let tabContent = document.querySelectorAll(".tab-content > div");
